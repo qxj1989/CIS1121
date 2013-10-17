bccc5a9290401bcb031227bbb3a082e1468280d1
qxj1989 <xinjianqi@yahoo.com>
2013-10-16 22:19:49 -0400

    add new file

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>My First Site</title>
+</head>
+<body>
+	<div id="header">
+	<a id= "headerContent">first</a>
+	<a id= "headerContent">first</a>
+	<a id= "headerContent">first</a>
+	<a id= "headerContent">first</a>
+	</div>
+</body>
+</html>
